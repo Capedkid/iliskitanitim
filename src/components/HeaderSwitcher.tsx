0b96bbe46d6ce1abcb342c95b5243591dfede7df
Capedkid <muhammed.kalp123@gmail.com>
2025-10-16 17:50:18 +0300

    login page update

--- src/components/HeaderSwitcher.tsx
+++ src/components/HeaderSwitcher.tsx
@@ -6,5 +6,6 @@ import HeaderGlobal from "@/components/HeaderGlobal";
 export default function HeaderSwitcher() {
   const pathname = usePathname();
   if (pathname === "/") return <HeaderHome />;
+  if (pathname?.startsWith("/login")) return null;
   return <HeaderGlobal />;
 }
